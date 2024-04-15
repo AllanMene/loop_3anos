2a67f47dc21fb903bd90af3f52f9c4307c8f6e25
Allan Meneguetti <106668419+AllanMene@users.noreply.github.com>
2024-04-15 08:47:27 -0300

    pal

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("Que time foi o primeiro campeão mundial em 1951?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
+    if (respostaTime.toLowerCase() === "palmeiras") {
       alert("Isso mesmo! O Athlético PR é o melhor time!");
-      continuarPerguntando = false; // Sai do loop após a resposta correta
+      continuarPerguntando = true; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
     }
